0ccdc2e985389d58ea9e68dfe3d3f51e0fd667f6
Willian Yassunaka <willianhy@gmail.com>
2017-07-29 15:50:00 -0300

    Tipos em go

--- hello/hello.go
+++ hello/hello.go
@@ -3,5 +3,9 @@ package main
 import "fmt"
 
 func main(){
-    fmt.Println("Olá Mundo!")
+    nome := "João"
+    idade := 20
+    versao := 1.1
+    fmt.Println("Olá, sr.", nome, "sua idade é", idade)
+    fmt.Println("Este programa está na versão", versao)
 }
