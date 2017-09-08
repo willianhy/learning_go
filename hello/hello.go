1dc75f201573b1eb732c5e7a9793cf9127834a09
Willian Yassunaka <willian.yassunaka@benner.com.br>
2017-09-08 13:26:23 -0300

    refactoring to methods

--- hello/hello.go
+++ hello/hello.go
@@ -1,31 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
-func main(){
-    nome := "Douglas"
-    versao := 1.1
+func main() {
+	exibeIntroducao()
+	exibeMenu()
 
-    fmt.Println("Olá, sr(a).", nome)
-    fmt.Println("Este programa está na versão", versao)
+	switch leComando() {
+	case 1:
+		fmt.Println("Monitorando...")
+	case 2:
+		fmt.Println("Exibindo logs...")
+	case 0:
+		fmt.Println("Saindo do programa")
+		os.Exit(0)
+	default:
+		fmt.Println("Não conheço este comando")
+		os.Exit(-1)
+	}
+}
 
-    fmt.Println("1- Iniciar Monitoramento")
-    fmt.Println("2- Exibir Logs")
-    fmt.Println("0- Sair do Programa")
+func exibeIntroducao() {
+	nome := "Douglas"
+	versao := 1.1
 
-    var comando int
-    fmt.Scan(&comando)
+	fmt.Println("Olá, sr(a).", nome)
+	fmt.Println("Este programa está na versão", versao)
+}
 
-    fmt.Println("O valor da variável comando é:", comando)
+func exibeMenu() {
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair do Programa")
+}
 
-    switch comando {
-    case 1:
-        fmt.Println("Monitorando...")
-    case 2:
-        fmt.Println("Exibindo logs...")
-    case 0:
-        fmt.Println("Saindo do programa")
-    default:
-        fmt.Println("Não conheço este comando")
-    }
+func leComando() int {
+	var comandoLido int
+	fmt.Scan(&comandoLido)
+	fmt.Println("O valor da variável comando é:", comandoLido)
+
+	return comandoLido
 }
